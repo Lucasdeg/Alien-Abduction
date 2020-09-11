43bb24093665de1e9d8f96ba36c95b4a44d8542d
Lucas de Graaf <lucas.98@hotmail.nl>
2020-09-11 21:34:40 +0200

    Added asteroids
    Customised main menu
    Imported new textures
    - player
    - asteroid
    -background

--- Alien Abduction/Assets/Scripts/GameManager.cs
+++ Alien Abduction/Assets/Scripts/GameManager.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject[] SpawnItems;
+
+    public Vector2 position;
     void Start()
     {
-        
+        InvokeRepeating("SpawnEnemy", 1, 1);
     }
 
-    void Update()
-    {
-        
+    void SpawnEnemy() {
+        int rnd = Random.Range(0, SpawnItems.Length);
+        // GameObject Camera = GameObject.Find("Main Camera");
+        Camera oCamera = Camera.main;
+        // float cameraHeight = 2f * oCamera.orthographicSize;
+        Vector2 position = new Vector2(Random.Range(-4.6f, 4.6f), oCamera.orthographicSize + 0.4f);
+        var debris = Instantiate(SpawnItems[0], position, Quaternion.identity);
+        GameObject Foreground = GameObject.Find("Foreground"); 
+        debris.transform.SetParent(Foreground.transform, false);
     }
 }
---
+++ Alien Abduction/Assets/Scripts/MovePhysics.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePhysics : MonoBehaviour
+{
+    public float Speed = -1;
+
+    void Start() {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(Random.Range(-1.0f, 1.0f), Speed);
+    }
+
+
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+
+    private void Update() {
+
+    }
+}
